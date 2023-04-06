530b1faeb6edaa0e665aaf87a81d08732ebf0a86
Sukrid_O <jobzakung@protonmail.com>
2023-04-06 21:24:14 +0700

    มันดี :sparkles:

--- src/ConnectionDB/database.sql
+++ src/ConnectionDB/database.sql
@@ -1,39 +1,34 @@
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "product" (
-	"id"	INTEGER,
-	"name"	TEXT NOT NULL,
-	"description"	TEXT,
-	"price"	REAL NOT NULL,
-	"category"	TEXT NOT NULL,
-	PRIMARY KEY("id")
+CREATE TABLE products (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT,
+  price REAL NOT NULL,
+  category TEXT NOT NULL
 );
-CREATE TABLE IF NOT EXISTS "customer" (
-	"id"	INTEGER,
-	"first_name"	TEXT NOT NULL,
-	"last_name"	TEXT NOT NULL,
-	"email"	TEXT NOT NULL UNIQUE,
-	"phone"	TEXT,
-	"address"	TEXT,
-	"city"	TEXT,
-	"state"	TEXT,
-	"zip"	TEXT,
-	PRIMARY KEY("id")
+
+CREATE TABLE customers (
+	id INTEGER PRIMARY KEY,
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	Email TEXT NOT NULL UNIQUE,
+	Phone TEXT,
+	address TEXT,
+	city TEXT,
+	state TEXT,
+	zip TEXT
 );
-CREATE TABLE IF NOT EXISTS "user" (
-	"id"	INTEGER,
-	"username"	TEXT NOT NULL UNIQUE,
-	"password"	TEXT NOT NULL,
-	"first_name"	TEXT NOT NULL,
-	"last_name"	TEXT NOT NULL,
-	"email"	TEXT NOT NULL UNIQUE,
-	PRIMARY KEY("id")
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  username TEXT NOT NULL,
+  password TEXT NOT NULl,
+  first_name TEXT NOT NULL,
+  Email TEXT NOT NULL UNIQUE
 );
-CREATE TABLE IF NOT EXISTS "order_by" (
-	"id"	INTEGER,
-	"order_date"	TEXT NOT NULL,
-	"customer_id"	INTEGER NOT NULL,
-	"Total"	DECIMAL(10, 2) NOT NULL,
-	FOREIGN KEY("customer_id") REFERENCES "customer"("id"),
-	PRIMARY KEY("id")
+
+CREATE TABLE orders (
+  id INTEGER PRIMARY KEY,
+  orders_data TEXT NOT NULL,
+  customers_id INTEGER NOT NULL,
+  Total DECIMAL(10, 2) NOT NULL
 );
-COMMIT;
